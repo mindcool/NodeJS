c2ba98606ddb91c78dda2a231f4cb0a507dc1359
Mehmet Noyan AYDIN <noyan@Mehmets-MacBook-Pro.local>
2017-01-07 23:56:11 -0700

    Created package JSON Created bower.JSON Created bowerrc to decide where bower directories will be saved Download a bootstrap template from bootstrapzero

--- library-project/app.js
+++ library-project/app.js
@@ -3,6 +3,18 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static('public'));
+
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+app.get('/books', function(req, res) {
+
+});
+
 app.listen(port, function() {
 	console.log('Running server on port: ', port);
 });
